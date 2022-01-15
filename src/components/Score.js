96e1f766003675cbe097822b688f090ca38da504
ricardonyc <rvardo98@gmail.com>
2022-01-15 13:38:34 -0500

    removed comment

--- src/components/Score.js
+++ src/components/Score.js
@@ -4,7 +4,6 @@ function Score(props) {
   return (
     <div className="score-container">
       {props.scores.map((score) => {
-        return (
             // <h4>Scores:</h4>
           <p>{score.date}: {score.score}</p>
         );
